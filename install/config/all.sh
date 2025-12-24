246d905e772e792b92e6ad3daee72796478a0e5e
Luke Kingland <luke@lukekingland.com>
2025-12-24 21:26:15 +0900

    yubikey and ssh

--- install/config/all.sh
+++ install/config/all.sh
@@ -3,6 +3,8 @@ run_logged $OMARCHY_INSTALL/config/theme.sh
 run_logged $OMARCHY_INSTALL/config/branding.sh
 run_logged $OMARCHY_INSTALL/config/git.sh
 run_logged $OMARCHY_INSTALL/config/gpg.sh
+run_logged $OMARCHY_INSTALL/config/gpg-agent.sh
+run_logged $OMARCHY_INSTALL/config/ssh.sh
 run_logged $OMARCHY_INSTALL/config/timezones.sh
 run_logged $OMARCHY_INSTALL/config/increase-sudo-tries.sh
 run_logged $OMARCHY_INSTALL/config/increase-lockout-limit.sh
